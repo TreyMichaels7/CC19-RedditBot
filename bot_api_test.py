7b91d7df3e33e206cec3c24a9a7bb580f9c141fd
treymichaels <thomam7@uw.edu>
2020-08-25 22:21:13 -0700

    Add response functionality for top counties

--- bot_api_test.py
+++ bot_api_test.py
@@ -1,8 +1,12 @@
 import requests
 
-resp = requests.get('https://covercovid-19.com/reports/official/total')
+resp = requests.get('https://covercovid-19.com/reports/counties/top')
 if resp.status_code != 200:
     # This means something went wrong.
     raise ApiError('GET /tasks/ {}'.format(resp.status_code))
 else:
-    print(resp.text)
+    counties = resp.json()
+    body = ""
+    for i, county in enumerate(counties, start =1):
+        body +=  "\n" + str(i) + ". " + (county['name'] + ", " + county['state'] + ": " + str(county['2wd']))
+    print(body)
--- bot_post.py
+++ bot_post.py
@@ -26,18 +26,6 @@ for submission in subreddit.hot(limit=10):
         # If we haven't replied to this comment before
         if comment.id not in posts_replied_to:
 
-            # Test
-            # Do a case insensitive search
-            '''
-            if re.search("i love python", comment.body, re.IGNORECASE):
-                # Reply to the post
-                comment.reply("Nigerian scammer bot says: It's all about the Bass (and Python)")
-                print("Bot replying to : ", comment.author)
-
-                # Store the current id into our list
-                posts_replied_to.append(comment.id)
-            '''
-
             # Print the Bot Summary and Command List
             if re.search("CoverCovid !Help", comment.body, re.IGNORECASE):
                 body = """
@@ -65,7 +53,7 @@ I am a bot for **CoverCovid-19** which tracks Covid-19 cases in the **United Sta
             if re.search("CoverCovid !Total", comment.body, re.IGNORECASE):
                 resp = requests.get('https://covercovid-19.com/reports/official/total')
                 totalCount = resp.text
-                body = "The total number of cases in the United States currently is approximately  **{}**. For more detailed information: visit [CoverCovid-19.com](https://covercovid-19.com/).".format(totalCount)
+                body = "The total number of cases in the United States currently is approximately  **{}**. For more detailed information: visit [CoverCovid-19.com](https://covercovid-19.com/). ".format(totalCount)
                 comment.reply(body)
                 print("Bot replying to : ", comment.author)
                 posts_replied_to.append(comment.id)
@@ -73,7 +61,11 @@ I am a bot for **CoverCovid-19** which tracks Covid-19 cases in the **United Sta
             # Prints the stats of the top 5 counties currently being impacted by Covid-19. 
             if re.search("CoverCovid !TopCounties", comment.body, re.IGNORECASE):
                 resp = requests.get('https://covercovid-19.com/reports/counties/top')
-                comment.reply("")
+                counties = resp.json()
+                body = ""
+                for i, county in enumerate(counties, start =1):
+                    body +=  "\n" + str(i) + ". " + (county['name'] + ", " + county['state'] + ": **" + str(county['2wd']) + "**")
+                comment.reply(body)
                 print("Bot replying to : ", comment.author)
                 posts_replied_to.append(comment.id)
 
