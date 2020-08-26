c8bd4c4afd160c531cc6e4ac632dc5785d491475
treymichaels <thomam7@uw.edu>
2020-08-25 21:30:14 -0700

    Total case done

--- bot_api_test.py
+++ bot_api_test.py
@@ -1,8 +1,8 @@
 import requests
 
-resp = requests.get('https://covercovid-19.com/reports/self')
+resp = requests.get('https://covercovid-19.com/reports/official/total')
 if resp.status_code != 200:
     # This means something went wrong.
     raise ApiError('GET /tasks/ {}'.format(resp.status_code))
 else:
-    print(resp.json())
+    print(resp.text)
--- bot_post.py
+++ bot_post.py
@@ -18,7 +18,7 @@ else:
         posts_replied_to = list(filter(None, posts_replied_to))
 
 # Get the top 5 values from our subreddit
-subreddit = reddit.subreddit('Coronavirus')
+subreddit = reddit.subreddit('pythonforengineers')
 # Check each submission in the subreddit
 for submission in subreddit.hot(limit=10):
     # Check each comment for each submission
@@ -28,6 +28,7 @@ for submission in subreddit.hot(limit=10):
 
             # Test
             # Do a case insensitive search
+            '''
             if re.search("i love python", comment.body, re.IGNORECASE):
                 # Reply to the post
                 comment.reply("Nigerian scammer bot says: It's all about the Bass (and Python)")
@@ -35,6 +36,7 @@ for submission in subreddit.hot(limit=10):
 
                 # Store the current id into our list
                 posts_replied_to.append(comment.id)
+            '''
 
             # Print the Bot Summary and Command List
             if re.search("CoverCovid !Help", comment.body, re.IGNORECASE):
@@ -42,12 +44,12 @@ for submission in subreddit.hot(limit=10):
                 I am a bot for **CoverCovid-19** which tracks Covid-19 cases in the **United States**. I cover cases on both a local (self-reported) and county level (officially reported). I can also provide links to the official CoverCovid-19 Covid-19 map.  
       
                 ---
-                **CoverCovid! Help** - Prints the bot summary and command list
-                **CoverCovid! Total** - Prints the current approximate total of cases in the US.
-                **CoverCovid! TopCounties** - Prints the stats of the top 5 counties currently being impacted by Covid-19.
-                **CoverCovid! Map** - Provides a link to the CoverCovid-19 Covid Tracker map for viewing or self-reporting.
-                **CoverCovid! Map CountyName** - Provides a link to the CoverCovid-19 map centered on the provided county, if it exists. If the county does not exist, then it will provide a link to the map with its default view.
-                **CoverCovid! StateMap** - Provides a link to the official CDC map for US state Covid-19 stats.
+                **CoverCovid !Help** - Prints the bot summary and command list
+                **CoverCovid !Total** - Prints the current approximate total of cases in the US.
+                **CoverCovid !TopCounties** - Prints the stats of the top 5 counties currently being impacted by Covid-19.
+                **CoverCovid !Map** - Provides a link to the CoverCovid-19 Covid Tracker map for viewing or self-reporting.
+                **CoverCovid !Map CountyName** - Provides a link to the CoverCovid-19 map centered on the provided county, if it exists. If the county does not exist, then it will provide a link to the map with its default view.
+                **CoverCovid !StateMap** - Provides a link to the official CDC map for US state Covid-19 stats.
                 '''
                 comment.reply(body)
                 print("Bot replying to : ", comment.author)
@@ -56,11 +58,8 @@ for submission in subreddit.hot(limit=10):
             # Prints the current approximate total of cases in the US.
             if re.search("CoverCovid !Total", comment.body, re.IGNORECASE):
                 resp = requests.get('https://covercovid-19.com/reports/official/total')
-                totalCount = str(resp.json())
-                body = '''
-                The total number of cases in the United States currently is approximately  **$totalCount**. 
-                For more detailed information: visit [CoverCovid-19.com](https://covercovid-19.com/).
-                '''
+                totalCount = resp.text
+                body = "The total number of cases in the United States currently is approximately  **{}**. For more detailed information: visit [CoverCovid-19.com](https://covercovid-19.com/).".format(totalCount)
                 comment.reply(body)
                 print("Bot replying to : ", comment.author)
                 posts_replied_to.append(comment.id)
