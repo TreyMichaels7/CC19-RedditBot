8914ecea9798877851292ac3ed4af33063c169d6
treymichaels <thomam7@uw.edu>
2020-09-07 15:37:02 -0700

    Finish responses for CoverCovid-19 bot

--- bot_api_test.py
+++ bot_api_test.py
@@ -1,12 +1,14 @@
 import requests
 
-resp = requests.get('https://covercovid-19.com/reports/counties/top')
+resp = requests.get('https://covercovid-19.com/reports/counties/king/washington')
 if resp.status_code != 200:
     # This means something went wrong.
     raise ApiError('GET /tasks/ {}'.format(resp.status_code))
 else:
     counties = resp.json()
-    body = ""
-    for i, county in enumerate(counties, start =1):
-        body +=  "\n" + str(i) + ". " + (county['name'] + ", " + county['state'] + ": " + str(county['2wd']))
-    print(body)
+    if counties is None:
+        print("Uh-Oh, it looks like you might have mistyped the county name. Click [here](https://covercovid-19.com/#map-anchor) to view the whole interactive map of US Covid-19 statistics.")
+    else:
+        lat = counties["latitude"]
+        lon = counties["longitude"]
+        print("Click [here](https://covercovid-19.com/?lat={}&lon={})".format(lat, lon))
--- bot_post.py
+++ bot_post.py
@@ -29,7 +29,7 @@ for submission in subreddit.hot(limit=10):
             # Print the Bot Summary and Command List
             if re.search("CoverCovid !Help", comment.body, re.IGNORECASE):
                 body = """
-I am a bot for **CoverCovid-19** which tracks Covid-19 cases in the **United States**. I cover cases on both a local (self-reported) and county level (officially reported). I can also provide links to the official CoverCovid-19 Covid-19 map.
+I am a bot for **CoverCovid-19** which tracks Covid-19 cases in the **United States**. I cover cases on both a local (self-reported) and county level (officially reported by the NYT). I can also provide links to the official [CoverCovid-19](https://covercovid-19.com) Covid-19 map.
 
 ---
 
@@ -41,9 +41,14 @@ I am a bot for **CoverCovid-19** which tracks Covid-19 cases in the **United Sta
 
 **CoverCovid !Map** \- Provides a link to the CoverCovid-19 Covid Tracker map for viewing or self-reporting.  
 
-**CoverCovid !Map CountyName** \- Provides a link to the CoverCovid-19 map centered on the provided county, if it exists. If the county does not exist, then it will provide a link to the map with its default view.  
+**CoverCovid !County <County Name>, <State Name>** \- Provides a link to the CoverCovid-19 map centered on the provided county, if it exists. If the county does not exist, then it will provide a link to the map with its default view.  
 
 **CoverCovid !StateMap** \- Provides a link to the official CDC map for US state Covid-19 stats.
+
+---
+Beep Boop, I am a Bot. 
+Comment CoverCovid! Help for a list of my commands!
+[Click here to send me feedback!](https://www.reddit.com/message/compose/?to=CoverCovid19)
                 """
                 comment.reply(body)
                 print("Bot replying to : ", comment.author)
@@ -53,7 +58,13 @@ I am a bot for **CoverCovid-19** which tracks Covid-19 cases in the **United Sta
             if re.search("CoverCovid !Total", comment.body, re.IGNORECASE):
                 resp = requests.get('https://covercovid-19.com/reports/official/total')
                 totalCount = resp.text
-                body = "The total number of cases in the United States currently is approximately  **{}**. For more detailed information: visit [CoverCovid-19.com](https://covercovid-19.com/). ".format(totalCount)
+                body = """The total number of cases in the United States currently is approximately  **> 6300000**. For more detailed information: visit [CoverCovid-19.com](https://covercovid-19.com/).
+ 
+---
+Beep Boop, I am a Bot. 
+Comment CoverCovid! Help for a list of my commands!
+[Click here to send me feedback!](https://www.reddit.com/message/compose/?to=CoverCovid19)
+                """.format(totalCount)
                 comment.reply(body)
                 print("Bot replying to : ", comment.author)
                 posts_replied_to.append(comment.id)
@@ -65,27 +76,66 @@ I am a bot for **CoverCovid-19** which tracks Covid-19 cases in the **United Sta
                 body = ""
                 for i, county in enumerate(counties, start =1):
                     body +=  "\n" + str(i) + ". " + (county['name'] + ", " + county['state'] + ": **" + str(county['2wd']) + "**")
+                body += """
+
+---
+Beep Boop, I am a Bot. 
+Comment CoverCovid! Help for a list of my commands!
+[Click here to send me feedback!](https://www.reddit.com/message/compose/?to=CoverCovid19)
+                """
                 comment.reply(body)
                 print("Bot replying to : ", comment.author)
                 posts_replied_to.append(comment.id)
 
             # Provides a link to the CoverCovid-19 Covid Tracker map for viewing or self-reporting. 
             if re.search("CoverCovid !Map", comment.body, re.IGNORECASE):
-                comment.reply("Click [here](https://covercovid-19.com) to view an interactive map of US Covid-19 statistics or to report that you feel unwell and suspect having Covid-19.")
+                body = "Click [here](https://covercovid-19.com/#map-anchor) to view an interactive map of US Covid-19 statistics or to report that you feel unwell and suspect having Covid-19."
+                body += """
+
+---
+Beep Boop, I am a Bot. 
+Comment CoverCovid! Help for a list of my commands!
+[Click here to send me feedback!](https://www.reddit.com/message/compose/?to=CoverCovid19)
+                """
+                comment.reply(body)
                 print("Bot replying to : ", comment.author)
                 posts_replied_to.append(comment.id)
 
             # Provides a link to the CoverCovid-19 map centered on the provided county, if it exists. 
             # If the county does not exist, then it will provide a link to the map with its default view. 
-            if re.search("CoverCovid !Map <County>", comment.body, re.IGNORECASE):
-                resp = requests.get('https://covercovid-19.com/reports/counties/<County>')
-                comment.reply("Click here to find statistics about Covid-19 in <County> or to report that you feel unwell and suspect having Covid-19.")
+            if re.search("CoverCovid !County [a-zA-Z]+, [a-zA-Z]+", comment.body, re.IGNORECASE):
+                responseArr = comment.body.split("CoverCovid !County ")[1]
+                data = responseArr.split(",")
+                county = data[0]
+                state = data[1].strip(" ")
+                resp = requests.get('https://covercovid-19.com/reports/counties/{}/{}'.format(county, state))
+                body = ""
+                if resp is None:
+                    body = "Uh-Oh, it looks like you might have mistyped the county name. Click [here](https://covercovid-19.com/#map-anchor) to view the whole interactive map of US Covid-19 statistics."
+                else:
+                    body = "Click [here](https://covercovid-19.com/?lat={}&lon={}) to find statistics about Covid-19 in {}, {} or to report that you feel unwell and suspect having Covid-19.".format(county, state, county, state)
+                body += """
+
+---
+Beep Boop, I am a Bot. 
+Comment CoverCovid! Help for a list of my commands!
+[Click here to send me feedback!](https://www.reddit.com/message/compose/?to=CoverCovid19)
+                """
+                comment.reply(body)
                 print("Bot replying to : ", comment.author)
                 posts_replied_to.append(comment.id)
 
             # Provides a link to the official CDC map for US state Covid-19 stats. 
             if re.search("CoverCovid !StateMap", comment.body, re.IGNORECASE):
-                comment.reply("Click [here](https://covercovid-19.com#map-cdc) to view an interactive map of US Covid-19 statistics by state.")
+                body = "Click [here](https://covercovid-19.com#map-cdc) to view an interactive map of US Covid-19 statistics by state."
+                body += """
+
+---
+Beep Boop, I am a Bot. 
+Comment CoverCovid! Help for a list of my commands!
+[Click here to send me feedback!](https://www.reddit.com/message/compose/?to=CoverCovid19)
+                """
+                comment.reply(body)
                 print("Bot replying to : ", comment.author)
                 posts_replied_to.append(comment.id)
 
